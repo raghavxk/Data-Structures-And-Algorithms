b2046c1faae2320ec5766bee3dc1c2340cea0419
Raghavendra Khare <raghavxk@gmail.com>
2021-08-19 12:55:41 +0530

    code revised

--- recursion/tower_of_hanoi.cpp
+++ recursion/tower_of_hanoi.cpp
@@ -1,5 +1,4 @@
-void towerOfHanoi(int n, char from_rod,
-                    char to_rod, char aux_rod)
+void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
 {
     if (n == 1)
     {
