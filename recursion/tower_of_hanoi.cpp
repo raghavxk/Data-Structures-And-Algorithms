244c23af08daac95fe334a7c421cfe248b735480
Raghavendra Khare <raghavxk@gmail.com>
2021-08-19 12:54:47 +0530

    efforts

---
+++ recursion/tower_of_hanoi.cpp
@@ -0,0 +1,12 @@
+{
+    if (n == 1)
+    {
+        cout << "Move disk 1 from rod " << from_rod <<
+                            " to rod " << to_rod<<endl;
+        return;
+    }
+    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+    cout << "Move disk " << n << " from rod " << from_rod <<
+                                " to rod " << to_rod << endl;
+    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+}
